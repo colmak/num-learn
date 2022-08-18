b01ff90ad4cb2bc3283b0c63c858b5c733a31a2b
colmak <69098858+colmak@users.noreply.github.com>
2022-08-18 17:46:10 -0400

    Generate t3 starter app and add color pallete

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      'bg': '#242933',
+      'main': '#ec4c56',
+      'caret': '#ec4c56',
+      'sub': '#596172',
+      'sub-alt': '#1c222d',
+      'text': '#f6f0e9',
+      'error': '#ec4c56',
+      'error-extra': '#9b333a',
+      'colorful-error': '#ecdc4c',
+      'colorful-error-extra': '#bdb03d',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
